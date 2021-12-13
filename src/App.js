338c73ea07a664671b64c8216494f283f95dd6d2
Aaron Bertell <aaronbertell@Aarons-MacBook-Pro.local>
2021-12-13 17:05:53 +0900

    movement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import './App.css'
-import Title from './Title/Title'
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Title from './Title/Title';
 import Skills from './Skills/Skills';
 
 function App() {
+  const [offSetY, setOffsetY] = useState(0);
+
+  useEffect( () => {
+
+    
+    window.addEventListener('scroll', handleScroll,true);
+ 
+    return window.removeEventListener('scroll', handleScroll);
+  })
+
+  const handleScroll = ()  => {
+    setOffsetY(window.pageYOffset);
+
+  } 
+  
   return (
-    <div className="App">
-      <Title></Title>
+    <div   className="App">
+      <Title offSetY={offSetY}></Title>
       <Skills></Skills>
     </div>
   );
--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -4,7 +4,7 @@ import './skills.css';
 function Skills () {
     return(
         <div className='skills'>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id provident vel sequi magni repellat, quo ipsum, dicta eum quas beatae sed, voluptas ipsa praesentium numquam facilis neque! Corporis, saepe modi.</div>
+            <div className='transition'></div>
         </div>
     );
 }
